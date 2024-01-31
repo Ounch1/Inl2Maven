81085470eaf748c4190fe61f621883c36348dbfd
Ilja Grigorjev <tomkapisos@gmail.com>
2024-01-31 10:20:34 +0100

    Implementing Book Test Methods

--- src/test/java/models/BookTest.java
+++ src/test/java/models/BookTest.java
@@ -8,14 +8,24 @@ class BookTest {
 
 	//G
 	@Test
-	public void test2EqualBooks() {
+	public void test2EqualBooks()
+	{
+		Book book1 = new Book(1, "Title", "Author", "ISBN123", "Branch", 200);
+		Book book2 = new Book(1, "Title", "Author", "ISBN123", "Branch", 200);
 
+		// Assert that the books are equal
+		assertEquals(book1, book2);
 	}
 
 	//G
 	@Test
-	public void test2NonEqualBooks() {
+	public void test2NonEqualBooks()
+	{
+		Book book1 = new Book(1, "Title", "Author", "ISBN123", "Branch", 200);
+		Book book2 = new Book(2, "Title", "Author", "ISBN123", "Branch", 200);
 
+		// Assert that the books are equal
+		assertNotEquals(book1, book2);
 	}
 
 }
