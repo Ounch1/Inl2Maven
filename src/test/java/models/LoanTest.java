8c10ac822fb4587880dabfe26e257d2cec4f3821
Ilja Grigorjev <tomkapisos@gmail.com>
2024-02-02 11:52:47 +0100

    Update LoanTest.java

--- src/test/java/models/LoanTest.java
+++ src/test/java/models/LoanTest.java
@@ -3,13 +3,19 @@ package models;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import utilities.GenderType;
 
 import java.time.LocalDate;
 
 public class LoanTest {
 	//VG
 	@Test
-	public void testDueDate() {
+	public void testDueDate()
+	{
+		Customer customer = new Customer("Mr", "Michael", "Smith", "1 The High Street","1234","a@b.com",1, GenderType.MALE);
+		Book book1 = new Book(1,"An introduction to Java","Matt Greencroft","12345","Anytown Branch", 400);
+		Loan loan = new Loan(1,customer,book1);
 
-   }
+		assertEquals(LocalDate.now().plusDays(14), loan.getDueDate());
+    }
 }
